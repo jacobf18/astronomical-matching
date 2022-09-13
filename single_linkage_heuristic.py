d313a99ac678083627002a1d93162fc8130ed875
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2022-09-13 00:30:43 -0400

    added single linkage heuristic method

--- single_linkage_heuristic.py
+++ single_linkage_heuristic.py
@@ -1,1 +1,68 @@
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+import matplotlib.pyplot as plt
+
+
+def neg_log_bayes_factor(cluster: np.array):
+    """
+    Calculate the negative log Bayes Factor for a given clustering.
+    
+    cluster is an numpy array with shape (n, 3) where n is the number of
+    sources in the cluster.
+    
+    The first column is the first coordinate, the second column is the second, and
+    the third column is the uncertainty.
+    
+    See Nguyen et al. 2022 for details on the Bayes factor.
+    """
+    num_objects = cluster.shape[0]
+
+    kappas = np.reciprocal(cluster.T[2] ** 2) # kappa = 1/sigma^2
+    sum_kappas = np.sum(kappas) # sum of all kappas
+    
+    # First 3 terms in formula
+    neg_bayes = (1 - num_objects) * np.log(2) - np.sum(np.log(kappas)) + np.log(sum_kappas)
+    
+    # Double sum in formula
+    pairwise_dist = pdist(cluster[:,:2], metric="sqeuclidean") # get pairwise squared distances
+    kappas_prod = kappas[:,None] * kappas # get pairwise products (vectorized)
+    np.fill_diagonal(kappas_prod, 0) # fill the diagonal with 0's for the next line
+    kappas_prod_square = squareform(kappas_prod) # convert to condensed distance vector
+    
+    # Add final term
+    neg_bayes += np.sum(kappas_prod_square * pairwise_dist) / (4 * sum_kappas)
+    
+    return neg_bayes
+
+
+def assign_cluster_neg_bayes(linkage_arr: np.array, data: np.array, num_clusters):
+    cluster_labels = fcluster(linkage_arr, t=num_clusters, criterion = "maxclust")
+    
+    neg_bayes = 0
+    for label in np.unique(cluster_labels):
+        cluster = data[cluster_labels == label,:]
+        neg_bayes += neg_log_bayes_factor(cluster)
+    return neg_bayes
+    
+
+def find_clusters_single_linkage(cmat: np.array):
+    """
+    Find clusters in the catalog data.
+    First, find a min spanning tree.
+    Then, remove edges greedily to maximize
+    the Bayes Factor.
+    """
+    data = np.concatenate(cmat, axis=0)
+    Z = linkage(data[:,:2], method="single")
+
+    bayes = []
+    for n in range(75,data.shape[0]):
+        bayes.append(assign_cluster_neg_bayes(Z, data, n))
+
+    plt.figure(figsize=(10,5))
+    plt.plot(bayes)
+    plt.show()
+    
+    
