3ba99bcc8af2a7e3019f221b76ffd2f880da2afa
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2022-10-19 00:02:44 -0400

    updated model to include log 2 term

--- utils.py
+++ utils.py
@@ -1,28 +1,21 @@
 import numpy as np
 from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score
 from scipy.optimize import linear_sum_assignment
+import random
+import string
+import math
+import time 
+import itertools
+from math import log as ln
+from math import exp
+import pandas as pd
+from gurobipy import *
+from scipy import spatial
+from scipy import sparse
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
 nmi = normalized_mutual_info_score
 ari = adjusted_rand_score
 
-
-def acc(y_true, y_pred):
-    """
-    Calculate clustering accuracy. Require scikit-learn installed
-    # Arguments
-        y: true labels, numpy.array with shape `(n_samples,)`
-        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
-    # Return
-        accuracy, in [0,1]
-    """
-    y_true = y_true.astype(np.int64)
-    assert y_pred.size == y_true.size
-    D = max(y_pred.max(), y_true.max()) + 1
-    w = np.zeros((D, D), dtype=np.int64)
-    for i in range(y_pred.size):
-        w[y_pred[i], y_true[i]] += 1
-    
-    ind = linear_sum_assignment(w.max() - w)
-    ind = np.array(list(zip(*ind)))
-    
-    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
